9a1b37e402f66af514fac268243706517b11cc49
Sachin <raisethebat2020@gmail.com>
2024-04-17 00:01:02 +0530

    filesystem learning

---
+++ fileSystem.js
@@ -0,0 +1,17 @@
+const fs=require('fs');
+
+
+fs.writeFile("hey.txt","zcnihidfpo",function(err){
+    if(err) console.log("error");
+    else console.log("done");
+})
+
+fs.appendFile("hey.txt","eesalacupnamde",function(err){
+    if(err) console.log("error");
+    else console.log("done");
+})
+
+fs.rename("hey.txt","bye.txt",function(err){
+    if(err) console.log("error");
+    else console.log("done");
+})
